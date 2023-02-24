434fd86c3bb1bab8201e3d65fc59b057e8feaf61
Sandiph Bamrel <49833723+whatevea@users.noreply.github.com>
2023-02-24 14:37:17 +0545

    added files

---
+++ script.js
@@ -0,0 +1,44 @@
+let h1=document.getElementById("animate_me");
+let search_btn=document.getElementById("button-addon2");
+function getMediaType(){
+   var radioButtons = document.getElementsByName("flexRadioDefault");
+  for(var i = 0; i < radioButtons.length; i++)
+    {
+      if (radioButtons[i].checked===true){
+        return radioButtons[i].value;
+      }
+      
+    }
+  
+}
+search_btn.onclick=()=>{
+  let query=document.getElementById("query_here").innerText;
+let type= getMediaType();
+ if(type="engish"){
+  let url=`https://fmovies.to/search?keyword=${query}`;
+   }
+  if(type="anime")
+{
+  let url=`https://9anime.to/filter?keyword=${query}`;
+}
+  else{
+    let url=`https://hindilinks4u.kim/?s=${query}`;
+  }
+  window.location=url;
+ //onclick end 
+  
+}
+
+// setInterval(change,400);
+
+  
+
+function change(){
+  console.log("iam inside loop");
+  if(!h1.innerText.includes("?????")){
+  h1.innerText+="?";
+}
+  else{
+    h1.innerText="What do you want to Watch ?";
+  }
+}
---
+++ vyzkwyj.markdown
@@ -0,0 +1,7 @@
+vYzKwyJ
+-------
+
+
+A [Pen](https://codepen.io/whatevea/pen/vYzKwyJ) by [sandip bamrel](https://codepen.io/whatevea) on [CodePen](https://codepen.io).
+
+[License](https://codepen.io/license/pen/vYzKwyJ).
